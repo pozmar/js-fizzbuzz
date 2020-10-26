800f258ea85a4db0516bc7c99cc7d674808af0bd
Martina <martina.pozzerle@libero.it>
2020-10-26 17:16:31 +0100

    #sistemato lista e condizioni

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var numUl = document.getElementById("lista");
 for (var i = 1; i <= 100; i++){
-  if(i % 3 == 0 && i % 5 === 0){
+  if(i % 3 === 0 && i % 5 === 0){
     numUl.innerHTML += "<li>" + "FizzBuzz" + "</li>";
   }
   else if (i % 5 === 0){
